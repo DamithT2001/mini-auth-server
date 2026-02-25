efd958a55bb409e0f1d18b0cdeb11ffe35c547b7
DamithT <damith.builtapps@gmail.com>
2026-02-25 10:51:49 +0530

    refactor ESLint configuration and enhance main.ts with global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   const logger = new (await import('@nestjs/common')).Logger('Bootstrap');
 
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;

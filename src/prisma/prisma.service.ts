740fc914997a8a026304e9269ba11edc972e68fa
D.M.D.T.B.Dissanayake <166440742+DamithT2001@users.noreply.github.com>
2026-02-26 10:27:33 +0530

    chore: prepare prisma 7 config and adapter-based prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,36 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma/client';
+
+type AdapterOptions = ConstructorParameters<typeof PrismaClient>[0];
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const options = PrismaService.buildAdapterOptions();
+    super(options);
+  }
+
+  private static buildAdapterOptions(): AdapterOptions | undefined {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { PrismaPg } = require('@prisma/adapter-pg');
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { Pool } = require('pg');
+
+      const pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+      });
+
+      const adapter = new PrismaPg(pool);
+      return { adapter };
+    } catch {
+      return undefined;
+    }
+  }
+
   async onModuleInit() {
     await this.$connect();
   }

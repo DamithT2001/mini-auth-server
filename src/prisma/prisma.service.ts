1b63fa05e5e9bafbf97d0986c3586146012cf6a2
DamithT <damith.builtapps@gmail.com>
2026-02-26 14:32:39 +0530

    enhance PrismaService to use connection pool for database management

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,20 +2,25 @@ import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  private readonly pool: Pool;
+
   constructor(private readonly configService: ConfigService) {
     const connectionString = configService.get<string>('DATABASE_URL');
     if (!connectionString) {
       throw new Error('DATABASE_URL is not set');
     }
 
-    const adapter = new PrismaPg({ connectionString });
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
     super({ adapter });
+    this.pool = pool;
   }
 
   async onModuleInit() {
@@ -24,5 +29,6 @@ export class PrismaService
 
   async onModuleDestroy() {
     await this.$disconnect();
+    await this.pool.end();
   }
 }
